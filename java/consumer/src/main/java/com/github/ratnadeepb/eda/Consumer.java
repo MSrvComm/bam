5a4e655341e33517eed13f71cd726c94d3a6cfd5
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2022-11-04 06:37:35 -0500

    +thread sleep

--- java/consumer/src/main/java/com/github/ratnadeepb/eda/Consumer.java
+++ java/consumer/src/main/java/com/github/ratnadeepb/eda/Consumer.java
@@ -22,23 +22,23 @@ import com.influxdb.client.InfluxDBClient;
 
 /**
  * Consumer
- * `
  */
 public class Consumer {
     final Logger mLogger = LoggerFactory.getLogger(Consumer.class.getName());
-    
 
-    String token = "3Ghjv-1exJYtV8U0no_u5zA9ikoa463f6B2Q5wUc2KN_n1dSuZnT3pVwfN57ZFs1eG6RnlWuHuOMJ1ze9qh2lw==";
+    String token = "E_HOFq8n1wkbEjuEeqi_fb4tqElN-GBH_VaTRAhxQfamMnQZWMIKS1ADzjCYBFl_26JFJWO4rYfOMKhIjud95w==";
     String bucket = "consumer";
     String org = "com.github.ratnadeepb";
     String url = "http://influxdb:8086";
 
     InfluxDBConnection dbconn = new InfluxDBConnection();
-    InfluxDBClient dbclient = dbconn.buildConnection(url, token, bucket, org);
+    InfluxDBClient dbclient = dbconn.buildConnection(url, token, bucket, org, mLogger);
 
     private class ConsumerRunnable implements Runnable {
         private CountDownLatch mLatch;
         private KafkaConsumer<Integer, Order> mConsumer;
+
+        private String containerIP = "container_ID:" + System.getenv("POD_IP");
 
         ConsumerRunnable(CountDownLatch latch) {
             mLatch = latch;
@@ -58,7 +58,8 @@ public class Consumer {
                                 Double value = (Double) metric.getValue().metricValue();
                                 mLogger.info("Reporting Metric: {}: {}", metric.getKey().name(),
                                         value);
-                                boolean res = dbconn.singlePointWrite(dbclient, "consumerID", value);
+                                mLogger.info("container IP: {}", containerIP);
+                                boolean res = dbconn.singlePointWrite(dbclient, containerIP, value);
                                 if (!res) {
                                     mLogger.info("failed to load db");
                                 }
@@ -72,11 +73,21 @@ public class Consumer {
                             int quantity = order.getQuantity();
                             String product = order.getProduct().toString();
 
-                            // if (key % 3 == 0) {
-                            // Thread.sleep(500);
-                            // }
+                            Integer sleepMS = 100;
 
-                            if (quantity < 2)
+                            if (key % 3 == 0) {
+                                if (Math.random() > 0.1) {
+                                    sleepMS = 500;
+                                }
+                            } else {
+                                if (Math.random() > 0.6) {
+                                    sleepMS = 500;
+                                }
+                            }
+
+                            Thread.sleep(sleepMS);
+
+                            if (quantity < 2 && product != "Windows")
                                 mLogger.info("Key: {}, Customer {} ordered {} {}", key, customerName, quantity,
                                         product);
                             else
@@ -85,10 +96,8 @@ public class Consumer {
                         }
                     }
                 }
-            } catch (WakeupException w) {
+            } catch (WakeupException | InterruptedException w) {
                 mLogger.info("Received shutdown signal");
-                // } catch (InterruptedException e) {
-                // e.printStackTrace();
             } finally {
                 mConsumer.close();
                 mLatch.countDown();
@@ -107,8 +116,11 @@ public class Consumer {
             props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "OrderGroup");
             props.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG,
                     CooperativeStickyAssignor.class.getName());
-            props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-            props.setProperty(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+            // props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+            // props.setProperty(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+            // props.setProperty(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "20000");
+            // props.setProperty(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, "22000");
+            // props.setProperty(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "5000");
             return props;
         }
     }
